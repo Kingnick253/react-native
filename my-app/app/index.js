f556b5184628533268f85efb64940572a14162b9
Kingnick253 <nicholaswebb2016@gmail.com>
2023-07-06 21:41:53 -0700

    getting started with configuring menu, and added more assets

--- my-app/app/index.js
+++ my-app/app/index.js
@@ -1,11 +1,31 @@
-import { View, Text } from 'react-native';
+import { View, ScrollView, SafeAreaView } from 'react-native';
+import {useState } from 'react';
+import { Stack, useRouter } from 'expo-router';
+
+import { COLORS, icons, images, SIZES } from '../constants' ;
+import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome} from '../components';
 
 const Home = () => {
+    // defining the router
+    const router = useRouter();
+    
     return (
-        // View = container, Text= P/H tag
-        <View>
-            <Text>Home </Text>
-        </View>
+        // SafeAreaView allows me to show the content without being covered by device hardware
+        <SafeAreaView style={{flex: 1, backgroundColor: COLORS.lightWhite}}>
+            <Stack.Screen
+                options={{headerStyle: {backgroundColor: COLORS.lightWhite}, 
+                          headerShadowVisible: false,
+                          headerLeft: () => (
+                            <ScreenHeaderBtn iconUrl={icons.menu} dimension ="60%" />
+                          ),
+                          headerRight: () => (
+                            <ScreenHeaderBtn iconUrl={icons.profile} dimension ="1000%" />
+                          ),
+                                          
+            }}
+            
+            />
+        </SafeAreaView>
     )
 }
 
