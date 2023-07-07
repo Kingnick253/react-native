5507abed52214a54aed46d3bb7ba51bb6e63c9b1
Kingnick253 <nicholaswebb2016@gmail.com>
2023-07-06 19:07:08 -0700

    added app folder and index/layout files

---
+++ my-app/app/index.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+
+const Home = () => {
+    return (
+        // View = container, Text= P/H tag
+        <View>
+            <Text>Home </Text>
+        </View>
+    )
+}
+
+export default Home;
