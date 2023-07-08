213da05f6b8e8b9c82c68f256e97190ba40f627a
Kingnick253 <nicholaswebb2016@gmail.com>
2023-07-07 20:44:41 -0700

    started messing around with some of the added fonts and playing with SplashScreen async and await

--- my-app/app/_layout.js
+++ my-app/app/_layout.js
@@ -1,5 +1,24 @@
 import { Stack } from "expo-router";    
+import { useCallback } from "react";
+import { useFont } from "expo-font";
+// this is importing everything
+import * as SplashScreen from 'expo-splash-screen';
+// Makes the native splash screen (configured in app.json) remain visible until hideAsync is called.
+SplashScreen.preventAutoHideAsync();
+
 const Layout = () => {
+    const [fontsLoaded] = useFonts({
+        DMBold: require('../assets/fonts/DMSans-Bold.ttf'),
+        DMMedium: require('../assets/fonts/DMSans-Medium.ttf'),
+        DMRegular: require('../assets/fonts/DMSans-Regular.ttf'),
+    })
+    const onLayoutRootView = useCallback(async ()=> {
+        if(fontsLoaded) {
+            await SplashScreen.hideAsync();
+        }
+
+    }, [fontsLoaded])
+
     return <Stack  />;
 }
 
--- my-app/components/home/welcome/Welcome.jsx
+++ my-app/components/home/welcome/Welcome.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
-import { View, Text } from 'react-native'
-
+import { useState } from 'react'
+// components
+import { View, Text, TextInput, TouchableOpacity, Image, FlatList } from 'react-native'
+import { useRouter } from 'expo-router';
+import {icons, SIZES } from '../../../constants'
 import styles from './welcome.style'
 
 const Welcome = () => {
+  // declaring router
+  const router = useRouter();
+
   return (
     <View>
-      <Text>Welcome</Text>
+      <View style={styles.container}>
+        <Text style={styles.userName}>Testing Hello World </Text>
+        <Text style={styles.welcomeMessage}>Find the Best Job for you </Text>
+      </View>
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+        <TextInput
+          style={styles.searchInput}
+        />
+       </View>
+      </View>
     </View>
   )
 }
