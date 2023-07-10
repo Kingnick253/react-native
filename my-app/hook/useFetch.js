89a03e684a083e78eb0a64c88f3f22ea3853c788
Kingnick253 <nicholaswebb2016@gmail.com>
2023-07-10 13:57:49 -0700

    added option function from rapid api, changed the search endpoint and prams to props, also hide apikey with env

--- my-app/hook/useFetch.js
+++ my-app/hook/useFetch.js
@@ -1,9 +1,24 @@
 import { useState, useEffect } from "react";
 import axios from 'axios';
+import {RAPID_API_KEY } from "@env";
 
-
-const useFecth = () => {
+const rapidApiKey = RAPID_API_KEY
+// endpoint is the prop im going to use for useFetch hook
+const useFecth = (endpoint, query) => {
     const [data, setData] = useState([]);
-    const [isLoading, setIsLoading ] =useState(false);
+    const [isLoading, setIsLoading ] = useState(false);
     const [error, setError ] = useState(null);
+    
+
+    // from rapidApi JSearch
+    const options = {
+        method: 'GET',
+        url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+        headers: {
+          'X-RapidAPI-Key': rapidApiKey,
+          'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
+        },
+        
+        params: {...query},
+      };
 }
