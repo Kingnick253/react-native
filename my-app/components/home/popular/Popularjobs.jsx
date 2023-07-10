6186f4f7619df21eeaa917514fdcdfd8ce71c37c
Kingnick253 <nicholaswebb2016@gmail.com>
2023-07-09 20:45:27 -0700

    sturcured if else statement for when the user is searching for Popular jobs

--- my-app/components/home/popular/Popularjobs.jsx
+++ my-app/components/home/popular/Popularjobs.jsx
@@ -1,22 +1,42 @@
-import React from 'react';
+
 import { useState } from 'react';
-import { View, Text, TouchableOpacity, Flatlist, ActivityIndicator } from 'react-native';
+import { View, Text, TouchableOpacity, FlatList, ActivityIndicator } from 'react-native';
 import { useRouter } from 'expo-router';
-import { COLOR, SIZES } from '../../../constants';
+import { COLORS, SIZES } from '../../../constants';
 import { PopularJobCard } from '../../common/cards/popular/PopularJobCard';
  
 import styles from './popularjobs.style'
 
 const Popularjobs = () => {
   const router = useRouter();
+  const isLoading = true;
+  const error = false;
 
   return (
+    // title section
     <View style={styles.container}>
       <View style={styles.header}>
       <Text style={styles.headerTitle}>Popular Jobs</Text>
       <TouchableOpacity>
         <Text style={styles.headerBtn}>Show All</Text>
       </TouchableOpacity>
+      </View>
+      {/* the rest of the content */}
+      <View style={styles.cardsContainer}>
+        
+        {isLoading ? (
+          <ActivityIndicator size="large" colors={COLORS.primary} />
+        ) : error ? (
+          <Text>Something went wrong </Text>
+        ) : (
+          <FlatList 
+          
+          
+          />
+        )}
+
+
+
       </View>
     </View>
   )
