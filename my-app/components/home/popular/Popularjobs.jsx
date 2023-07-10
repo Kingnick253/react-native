e1f32cbd9624f86464d636db18d8bf412118832e
Kingnick253 <nicholaswebb2016@gmail.com>
2023-07-09 20:27:53 -0700

    starting on the Popularjobs section

--- my-app/components/home/popular/Popularjobs.jsx
+++ my-app/components/home/popular/Popularjobs.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
-import { View, Text } from 'react-native'
-
+import React from 'react';
+import { useState } from 'react';
+import { View, Text, TouchableOpacity, Flatlist, ActivityIndicator } from 'react-native';
+import { useRouter } from 'expo-router';
+import { COLOR, SIZES } from '../../../constants';
+import { PopularJobCard } from '../../common/cards/popular/PopularJobCard';
+ 
 import styles from './popularjobs.style'
 
 const Popularjobs = () => {
+  const router = useRouter();
+
   return (
-    <View>
-      <Text>Popularjobs</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+      <Text style={styles.headerTitle}>Popular Jobs</Text>
+      <TouchableOpacity>
+        <Text style={styles.headerBtn}>Show All</Text>
+      </TouchableOpacity>
+      </View>
     </View>
   )
 }
